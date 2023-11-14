ed4c573c4fb6c6a2be62468b5dfc44875b0ed4fd
itc-s21016 <s21016@std.it-college.ac.jp>
2023-11-14 16:54:47 +0900

    カメラ起動から保存までを追加

--- app/src/main/java/com/example/sotukensanbaver20/MainActivity.kt
+++ app/src/main/java/com/example/sotukensanbaver20/MainActivity.kt
@@ -1,23 +1,49 @@
 package com.example.sotukensanbaver20
 
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.findNavController
 import com.example.sotukensanbaver20.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val textTitle = binding.titleText
+        /*val titleanimal = intent.getStringExtra("animal_key")
+
+        if (titleanimal == "animal") {
+            textTitle.setText(R.string.animalText)
+        }
+*/
+        binding.cameraBtn.setOnClickListener {
+            val intent = Intent(application,CameraActivity::class.java)
+            startActivity(intent)
+        }
+
         binding.illustratedBtn.setOnClickListener {
             val navController = findNavController(R.id.fragmentContainerView)
             navController.navigate(R.id.illustratedFragment)
+            textTitle.setText(R.string.illustratedText)
+        }
+
+
+
 
 //              動物画面確認用
 //            navController.navigate(R.id.action_mainFragment_to_animalFragment)
-        }
     }
+
 }
