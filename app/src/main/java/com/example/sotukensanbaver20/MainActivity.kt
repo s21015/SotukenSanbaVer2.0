e6448d15b0e18f88b71736d70fd9ec390f994511
itc-s21016 <s21016@std.it-college.ac.jp>
2023-11-28 16:03:48 +0900

    test

--- app/src/main/java/com/example/sotukensanbaver20/MainActivity.kt
+++ app/src/main/java/com/example/sotukensanbaver20/MainActivity.kt
@@ -8,7 +8,7 @@ import com.example.sotukensanbaver20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+//coment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
